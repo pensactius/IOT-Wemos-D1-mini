d011cc4cf5cb95d4e37aad16461db8a307a85187
PensActius <37923848+pensactius@users.noreply.github.com>
2018-04-01 08:33:36 +0000

    Update pub_mosquitto.py

--- tutorial/cap03/led_mosquitto/pub_mosquitto.py
+++ tutorial/cap03/led_mosquitto/pub_mosquitto.py
@@ -3,10 +3,11 @@
 import paho.mqtt.publish as publish
 import time
 
-print("Sending 0...")
-publish.single("ledStatus", "0", hostname="test.mosquitto.org")
+for i in range(10):
+  print("Sending 0...")
+  publish.single("ledStatus", "0", hostname="test.mosquitto.org")
 
-time.sleep(1)
+  time.sleep(1)
 
-print("Sending 1...")
-publish.single("ledStatus", "1", hostname="test.mosquitto.org")
+  print("Sending 1...")
+  publish.single("ledStatus", "1", hostname="test.mosquitto.org")
