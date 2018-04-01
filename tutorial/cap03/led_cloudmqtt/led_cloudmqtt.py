1666ccba4bfae095539cbf43f51eaf2a67393a2f
PensActius <37923848+pensactius@users.noreply.github.com>
2018-04-01 08:42:55 +0000

    Update led_cloudmqtt.py

--- tutorial/cap03/led_cloudmqtt/led_cloudmqtt.py
+++ tutorial/cap03/led_cloudmqtt/led_cloudmqtt.py
@@ -7,14 +7,14 @@ import time
 for i in range(10):
   print("Sending 0...")
   publish.single("ledStatus", "0", hostname="m23.cloudmqtt.com", 
-        auth={'username':"usuario", 'password':"password"},
+        auth={'username':"bowmxsdl", 'password':"DYbXI6WWOwMe"},
         port=14785)
 
   time.sleep(1)
 
   print("Sending 1...")
   publish.single("ledStatus", "1", hostname="m23.cloudmqtt.com",
-        auth={'username':"usuario", 'password':"password"},
+        auth={'username':"bowmxsdl", 'password':"DYbXI6WWOwMe"},
         port=14785)
 
   time.sleep(1)
