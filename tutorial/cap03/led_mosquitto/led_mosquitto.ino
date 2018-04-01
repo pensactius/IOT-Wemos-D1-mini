9591223faaa4a61ba48a3c7b1faa6a6824fac333
PensActius <37923848+pensactius@users.noreply.github.com>
2018-04-01 08:08:14 +0000

    Create led_mosquitto.ino

---
+++ tutorial/cap03/led_mosquitto/led_mosquitto.ino
@@ -0,0 +1,64 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+ 
+// Connect to the WiFi
+const char* ssid = "miSSID";        // Poner aquí la SSID de la WiFi
+const char* password = "miPasswd";  // Poner aquí el passwd de la WiFi
+const char* mqtt_server = "test.mosquitto.org";
+ 
+WiFiClient espClient;
+PubSubClient client(espClient);
+ 
+void callback(char* topic, byte* payload, unsigned int length) {
+ Serial.print("Message arrived [");
+ Serial.print(topic);
+ Serial.print("] ");
+ for (int i=0;i<length;i++) {
+  char receivedChar = (char)payload[i];
+  Serial.print(receivedChar);
+  if (receivedChar == '0')
+  // ESP8266 Huzzah outputs are "reversed"
+  digitalWrite(LED_BUILTIN, HIGH);
+  if (receivedChar == '1')
+   digitalWrite(LED_BUILTIN, LOW);
+  }
+  Serial.println();
+}
+ 
+ 
+void reconnect() {
+ // Loop until we're reconnected
+ while (!client.connected()) {
+ Serial.print("Attempting MQTT connection...");
+ // Attempt to connect
+ if (client.connect("ESP8266 Client")) {
+  Serial.println("connected");
+  // ... and subscribe to topic
+  client.subscribe("ledStatus");
+ } else {
+  Serial.print("failed, rc=");
+  Serial.print(client.state());
+  Serial.println(" try again in 5 seconds");
+  // Wait 5 seconds before retrying
+  delay(5000);
+  }
+ }
+}
+ 
+void setup()
+{
+ Serial.begin(9600);
+ 
+ client.setServer(mqtt_server, 1883);
+ client.setCallback(callback);
+ 
+ pinMode(LED_BUILTIN, OUTPUT);
+}
+ 
+void loop()
+{
+ if (!client.connected()) {
+  reconnect();
+ }
+ client.loop();
+}
