f4e5efa9b1c664c97698a98ec89cfbc8f8694475
PensActius <37923848+pensactius@users.noreply.github.com>
2018-04-03 12:01:36 +0200

    Add files via upload

---
+++ tutorial/esp8266/dht11_basico/dht11_basico.ino
@@ -0,0 +1,43 @@
+// Incluímos la librería
+#include <DHT.h>
+
+// Definimos el pin digital donde se conecta el sensor
+const int DHTPIN = 13;
+
+// Tipo de sensor
+const int DHTTYPE = DHT11;
+
+// Crea e inicializa una variable para el sensor DHT11
+DHT dht (DHTPIN, DHTTYPE);
+
+void setup() {
+  
+  Serial.begin (9600);
+  Serial.println ("DHT11 test...");
+
+  dht.begin();
+
+}
+
+void loop() {
+
+  // Wait a few seconds between measurements
+  delay (2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
+  float h = dht.readHumidity();
+
+  //Read temperature in Celsius (the default)
+  float t = dht.readTemperature();
+
+  // Check if any reads failed and exit early (to try again).
+  if ( isnan (h) || isnan (t) ) {
+    Serial.println ("Failed to read from DHT sensor, trying again...");
+    return;
+  }
+
+  Serial.print ("Humidity: "); Serial.print (h); Serial.print (" %\t");
+  Serial.print ("Temperature: "); Serial.print (t); Serial.println (" C ");
+ 
+}
