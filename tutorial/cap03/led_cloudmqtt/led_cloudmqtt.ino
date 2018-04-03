51ab00a330a11120f358e28cc3dc07c9309e4fb3
PensActius <37923848+pensactius@users.noreply.github.com>
2018-04-03 14:23:24 +0200

    Update led_cloudmqtt.ino

--- tutorial/cap03/led_cloudmqtt/led_cloudmqtt.ino
+++ tutorial/cap03/led_cloudmqtt/led_cloudmqtt.ino
@@ -31,21 +31,21 @@ void callback(char* topic, byte* payload, unsigned int length) {
  
 void reconnect() {
  // Loop until we're reconnected
- while (!client.connected()) {
- Serial.print("Attempting MQTT connection...");
- // Attempt to connect,               <<Usuario>> << Password >>
- if (client.connect("ESP8266 Client", "bowmxsdl", "DYbXI6WWOwMe")) {
-  Serial.println("connected");
-  // ... and subscribe to topic
-  client.subscribe("ledStatus");
- } else {
-  Serial.print("failed, rc=");
-  Serial.print(client.state());
-  Serial.println(" try again in 5 seconds");
-  // Wait 5 seconds before retrying
-  delay(5000);
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect,               <<Usuario>> << Password >>
+    if (client.connect("ESP8266 Client", "bowmxsdl", "DYbXI6WWOwMe")) {
+      Serial.println("connected");
+      // ... and subscribe to topic
+      client.subscribe("ledStatus");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
   }
- }
 }
  
 void setup()
